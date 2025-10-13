96d2141dc56499afe4decba39e9d3080b594fe92
tuhindutta <tuhindutta79@gmail.com>
2025-10-13 11:23:53 +0530

    Refactoring the scripts

--- src/train.py
+++ src/train.py
@@ -1,72 +1,78 @@
-import os
+# import os
 import yaml
 import mlflow
 from mlflow.models.signature import infer_signature
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
-import pickle
+# import pickle
 import argparse
 from utilities import Selector, CrossVal
 
 
-parser = argparse.ArgumentParser(description='Training script')
-parser.add_argument('-i', '--input', help='Specify input data csv path.')
-parser.add_argument('-c', '--config', help='Specify train config.')
-# parser.add_argument('-a', '--artifacts', help='Specify artifacts save path.')
-args = parser.parse_args()
-
-inp = args.input + '.csv' if not args.input.endswith('.csv') else args.input
-cnf = args.config + '.yaml' if not args.config.endswith('.yaml') else args.config
-# artifacts_path = args.artifacts
-
-with open(cnf, 'r') as file:
-    config = yaml.safe_load(file)
+def run_training(inp:str, cnf:str):
+    with open(cnf, 'r') as file:
+        config = yaml.safe_load(file)
 
 
-num_features = config['num_features']
-kfold_split = config['kfold_split']
-model_params = config['model_params']
+    num_features = config['num_features']
+    kfold_split = config['kfold_split']
+    model_params = config['model_params']
 
 
-df = pd.read_csv(inp)
-y = df.pop('sleep_disorder')
-X = df
+    df = pd.read_csv(inp)
+    y = df.pop('sleep_disorder')
+    X = df
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment('sleep_disorder')
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment('sleep_disorder')
 
-with mlflow.start_run():
+    with mlflow.start_run():
 
-    model = LogisticRegression(**model_params) if model_params is not None else LogisticRegression()
-    selec = Selector(model, num_features)
-    data = pd.DataFrame(selec.fit_transform(X, y), columns=selec.selector.get_feature_names_out())
-    model.fit(data, y)
-    kf = KFold(n_splits=kfold_split, shuffle=True, random_state=42)
-    cv = CrossVal(model, kf)
-    cv.fit(data, y)
-    cv_score = cv.cv_score
-    accuracy = cv.cv_score.get('accuracy')
-    roc_auc_ovr = cv.cv_score.get('roc_auc_ovr')
-    mlflow.log_param('num_features', num_features)
-    mlflow.log_param('kfold_split', kfold_split)
-    mlflow.log_metric('accuracy', accuracy)
-    mlflow.log_metric('roc_auc_ovr', roc_auc_ovr)
-    sig = infer_signature(data, model.predict(data))
-    model_info = mlflow.sklearn.log_model(
-        sk_model=model,
-        name="sleep-model",
-        signature= sig,
-        input_example=data.iloc[:5]
-    )
-    model_id = model_info.model_id
+        model = LogisticRegression(**model_params) if model_params is not None else LogisticRegression()
+        selec = Selector(model, num_features)
+        data = pd.DataFrame(selec.fit_transform(X, y), columns=selec.selector.get_feature_names_out())
+        model.fit(data, y)
+        kf = KFold(n_splits=kfold_split, shuffle=True, random_state=42)
+        cv = CrossVal(model, kf)
+        cv.fit(data, y)
+        cv_score = cv.cv_score
+        accuracy = cv.cv_score.get('accuracy')
+        roc_auc_ovr = cv.cv_score.get('roc_auc_ovr')
+        mlflow.log_param('num_features', num_features)
+        mlflow.log_param('kfold_split', kfold_split)
+        mlflow.log_metric('accuracy', accuracy)
+        mlflow.log_metric('roc_auc_ovr', roc_auc_ovr)
+        sig = infer_signature(data, model.predict(data))
+        model_info = mlflow.sklearn.log_model(
+            sk_model=model,
+            name="sleep-model",
+            signature= sig,
+            input_example=data.iloc[:5]
+        )
+
+    # selector_path = os.path.join(artifacts_path, 'feature_selector.pkl')
+    # with open(selector_path, 'wb') as file:
+    #     pickle.dump(selec, file)
+
+    # model_path = os.path.join(artifacts_path, 'model.pkl')
+    # with open(model_path, 'wb') as file:
+    #     pickle.dump(model, file)
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Training script')
+    parser.add_argument('-i', '--input', help='Specify input data csv path.')
+    parser.add_argument('-c', '--config', help='Specify train config.')
+    # parser.add_argument('-a', '--artifacts', help='Specify artifacts save path.')
+    args = parser.parse_args()
 
-# selector_path = os.path.join(artifacts_path, 'feature_selector.pkl')
-# with open(selector_path, 'wb') as file:
-#     pickle.dump(selec, file)
+    inp = args.input + '.csv' if not args.input.endswith('.csv') else args.input
+    cnf = args.config + '.yaml' if not args.config.endswith('.yaml') else args.config
+    # artifacts_path = args.artifacts
 
-# model_path = os.path.join(artifacts_path, 'model.pkl')
-# with open(model_path, 'wb') as file:
-#     pickle.dump(model, file)
+    run_training(inp, cnf)
+
+
+if __name__ == "__main__":
+    main()
